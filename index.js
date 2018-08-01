c6183169f023a369b10edbc4cf1af776ea6a4eab
BigThinkerYes <designaspnet4@hotmail.com>
2018-07-31 21:26:24 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = function(){
-  return <div>hi</div>;
+  return <div>hello</div>;
 }
 
+
+//instance of App
+//ReactDOM.render(<App />);
+//
 ReactDOM.render(<App />, document.querySelector('.container'));
