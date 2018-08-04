a4ed3cb1dcf1e1523ae5b7da0e97e27f455588c3
BigThinkerYes <designaspnet4@hotmail.com>
2018-08-04 14:31:55 -0400

    display user content in console
    title
    name 
    thumbnail
    information

--- index_1.3.js
+++ index_1.3.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-//use information from search_bar.js to import settings
-//.js files don't need file extension
-import SearchBar from './components/search_bar';
-
-const API_KEY = '';import React from 'react';
-import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 
@@ -37,32 +30,7 @@ const App = () => {
 ReactDOM.render(<App />, document.querySelector('.container'));
 
 
-//display information
-const App = () => {
-  return (
-         <div>
-              <SearchBar />
-         </div>
-         );
-  //include searchbar
-}
-
-
-//instance of App
-//ReactDOM.render(<App />);
-//
-ReactDOM.render(<App />, document.querySelector('.container'));
 
 
 
 
-
-
-/*const App = function(){
-  return <div>hello</div>;
-}
-//same as
-const App = () => {
-  return <div>hello</div>;
-}
-*/
